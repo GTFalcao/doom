6e243aa48fd7addbea7b8658edd37498453fb479
GTFalcao <gtfalcao96@gmail.com>
2023-11-27 00:06:23 -0300

    adding zip

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const fs = require("fs");
 const path = require("path");
 const { exec } = require("child_process");
+const { zip } = require("zip-a-folder");
 
 const accPath = path.join(__dirname, "./acc");
 const acsPath = path.join(__dirname, "./outcast/acs");
-const acsFiles = fs.readdirSync(acsPath)?.filter((file) => file.endsWith(".acs")) ?? [];
-acsFiles.forEach(file => {
-  const filePath = path.join(acsPath, file);
-  exec(`acc.exe ${filePath} ${filePath.replace(".acs", ".o")}`, { cwd: accPath }, (err, stdout, stderr) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    // console.log(stdout);
-    // console.log(stderr);
-  });
-})
+const acsFiles =
+  fs.readdirSync(acsPath)?.filter((file) => file.endsWith(".acs")) ?? [];
+
+async function build() {
+  await Promise.allSettled(
+    acsFiles.map(
+      (file) =>
+        new Promise((resolve) => {
+          const filePath = path.join(acsPath, file);
+          exec(
+            `acc.exe ${filePath} ${filePath.replace(".acs", ".o")}`,
+            { cwd: accPath },
+            (err, stdout, stderr) => {
+              if (err) {
+                console.log(err);
+              }
+              resolve();
+              // console.log(stdout);
+              // console.log(stderr);
+            }
+          );
+        })
+    )
+  );
+
+  await zip("/outcast", "dist/outcast.pk3");
+}
